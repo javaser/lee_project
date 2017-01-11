f47666e2878cfc7064cf32c70110da38b303d7a5
Xianzhan <424447832@qq.com>
2017-01-11 18:11:06 +0800

    快速排序

---
+++ lee_java/src/algorithm/sort/QuickSort.java
@@ -0,0 +1,42 @@
+package algorithm.sort;
+
+import algorithm.util.ArrayUtil;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = ArrayUtil.getIntArr();
+        ArrayUtil.printArr(arr, false);
+        sort(arr);
+        ArrayUtil.printArr(arr, true);
+    }
+
+    private static int getMiddle(int[] a, int left, int right) {
+        int tmp = a[left];    //数组的第一个作为中轴
+        while (left < right) {
+            while (left < right && a[right] >= tmp) {
+                right--;
+            }
+            a[left] = a[right];   //比中轴小的记录移到左边
+            while (left < right && a[left] <= tmp) {
+                left++;
+            }
+            a[right] = a[left];   //比中轴大的记录移到右边
+        }
+        a[left] = tmp;              //中轴记录到尾
+        return left;                   //返回中轴的位置
+    }
+
+    private static void quickSort(int[] a, int left, int right) {
+        if (left < right) {
+            int middle = getMiddle(a, left, right);  //将 a 数组进行一分为二
+            quickSort(a, left, middle - 1);       //对左边进行递归排序
+            quickSort(a, middle + 1, right);       //对右边进行递归排序
+        }
+    }
+
+    public static void sort(int[] a) {
+        if (a.length > 0) {    //查看数组是否为空
+            quickSort(a, 0, a.length - 1);
+        }
+    }
+}
