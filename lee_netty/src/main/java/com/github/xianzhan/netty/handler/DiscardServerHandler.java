a957c041ecb6f3b14a743b28beb2b74e840e6cb9
Xianzhan <424447832@qq.com>
2017-03-09 13:59:02 +0800

    DiscardServer Code

---
+++ lee_netty/src/main/java/com/github/xianzhan/netty/handler/DiscardServerHandler.java
@@ -0,0 +1,25 @@
+package com.github.xianzhan.netty.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // Discard the received data silently.
+//        ((ByteBuf) msg).release();
+        try {
+            // Do something with msg
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
