032148444ae57b6d9bf7eef5b33c8631cfd74572
Xianzhan <424447832@qq.com>
2017-03-25 12:41:28 +0800

    netty 权威指南，解决了 TCP 黏包、拆包问题

---
+++ lee_netty/src/main/java/com/github/xianzhan/learn/b/TimeServer.java
@@ -0,0 +1,58 @@
+package com.github.xianzhan.learn.b;
+
+import com.github.xianzhan.learn.b.handler.TimeServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class TimeServer {
+
+    public void bing(int port) throws Exception {
+        // 配置服务端的 NIO 线程组
+        // 一个用于服务端接受客户端的连接
+        // 一个用于进行 SocketChannel 的网络读写
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            // b 用于启动 NIO 服务端的辅助启动类，类似 JDK NIO 中的 ServerSocketChannel
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            socketChannel.pipeline().addLast(new StringDecoder());
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        System.out.println("The time server start in port: " + port);
+        new TimeServer().bing(port);
+    }
+}
