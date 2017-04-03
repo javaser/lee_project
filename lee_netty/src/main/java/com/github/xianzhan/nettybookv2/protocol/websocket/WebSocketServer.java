c3870f9845adf0344720b93539a71bd235857172
Xianzhan <424447832@qq.com>
2017-04-03 11:47:20 +0800

    netty WebSocket 协议实现

---
+++ lee_netty/src/main/java/com/github/xianzhan/nettybookv2/protocol/websocket/WebSocketServer.java
@@ -0,0 +1,51 @@
+package com.github.xianzhan.nettybookv2.protocol.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class WebSocketServer {
+    public void run(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline cp = socketChannel.pipeline();
+                            cp.addLast("http-codec", new HttpServerCodec());
+                            cp.addLast("aggregator", new HttpObjectAggregator(65536));
+                            cp.addLast("http-chunked", new ChunkedWriteHandler());
+                            cp.addLast("handler", new WebSocketServerHandler());
+                        }
+                    });
+            Channel ch = b.bind(port).sync().channel();
+            System.out.println("WebSocket server started at port: " + port);
+            System.out.println("Open your browser and navigate to " +
+                    "http://localhost:" + port + "/");
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        new WebSocketServer().run(port);
+    }
+}
